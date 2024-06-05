1ccaae79880ac6e4973a87c33cd527ca47f916c4
Braxton Mills <braxton@atlasagent.ai>
2024-06-05 02:23:58 -0500

    Optimize

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,88 +1,72 @@
-# Set $PSUserProfile
-$env:PSUserProfile = "~\OneDrive\Documents\PowerShell"
+# Set file paths
+$functionsPath = "$PSScriptRoot\Scripts\functions.ps1"
+$aliasesPath = "$PSScriptRoot\Scripts\aliases.ps1"
+$nvmPath = "$HOME\.nvm"
 
-. "$PSScriptRoot\Scripts\functions.ps1"
-. "$PSScriptRoot\Scripts\aliases.ps1"
-
-# Set NVM_DIR environment variable
-$env:NVM_DIR = "$HOME\.nvm"
-
-# Load nvm if available
-if (Test-Path "$env:NVM_DIR\nvm.ps1") {
-    . "$env:NVM_DIR\nvm.ps1"
+# Lazy-load functions and aliases
+function Import-MyFunctions {
+    . $functionsPath
 }
 
-# Load nvm bash_completion if available
-if (Test-Path "$env:NVM_DIR\nvm-bash-completion.ps1") {
-    . "$env:NVM_DIR\nvm-bash-completion.ps1"
+function Import-MyAliases {
+    . $aliasesPath
 }
 
-# Initialize zoxide
-$zoxidePath = (Get-Command zoxide).Source
+# Load NVM only when needed
+function Invoke-NVM {
+    $detectedFolders = @('node_modules')
+    $currentDirectory = Get-Location
 
-if (Test-Path $zoxidePath) {
-    $acl = Get-Acl $zoxidePath
-    $permissions = $acl.Access | Where-Object { $_.IdentityReference.Value -eq "$env:USERDOMAIN\$env:USERNAME" }
-    
-    if ($permissions.FileSystemRights -band [System.Security.AccessControl.FileSystemRights]::Read -and
-        $permissions.FileSystemRights -band [System.Security.AccessControl.FileSystemRights]::ExecuteFile) {
-        Invoke-Expression (& { (zoxide init powershell | Out-String) })
+    foreach ($folder in $detectedFolders) {
+        if (Test-Path -Path (Join-Path -Path $currentDirectory -ChildPath $folder)) {
+            if (Test-Path "$nvmPath\nvm.ps1") {
+                . "$nvmPath\nvm.ps1"
+            }
+            if (Test-Path "$nvmPath\nvm-bash-completion.ps1") {
+                . "$nvmPath\nvm-bash-completion.ps1"
+            }
+            break
+        }
     }
-    else {
-        Write-Output "Zoxide does not have the necessary permissions to run."
-    }
+}
+
+if (!(Test-Path "./node_modules" -PathType Container)) { 
+    write-host "-- Path not found"
 }
 else {
-    Write-Output "Zoxide is not accessible."
+    write-host "-- Path found"
 }
 
-
-# Import miniconda/conda/mamba configuration
-if (Test-Path "$PSScriptRoot\Scripts\conda_init.ps1") {
-    . "$PSScriptRoot\Scripts\conda_init.ps1"
+# Asynchronously load NVM
+$nvmLoader = @{
+    InvokeNVM = ${function:Invoke-NVM}
 }
 
-Import-Module -Name PSReadLine
-Import-Module Az.Tools.Predictor
-# Set PSReadLine key handlers and options
+$nvmLoader | ForEach-Object -Parallel {
+    # Set the necessary function in the parallel script block
+    $InvokeNVM = $_.InvokeNVM
+
+    # Invoke the Load-NVM function
+    & $InvokeNVM
+}
+
+# Asynchronously load modules
+$modules = @('PSReadLine', 'Az.Tools.Predictor', 'posh-git')
+
+# ForEach-Object -Parallel is a cmdlet introduced in PowerShell 7.0 that allows you to execute a script block in parallel for each input object.
+# It automatically distributes the processing of each object across multiple threads, enabling concurrent execution and potentially speeding up the processing time.
+$modules | ForEach-Object -Parallel {
+    Import-Module -Name $_
+}
+
+# Set PSReadLine options
 Set-PSReadLineKeyHandler -Chord Shift+Tab -Function TabCompleteNext
 Set-PSReadLineKeyHandler -Chord Tab -Function ForwardChar
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-# Github copilot alias
-$GH_COPILOT_PROFILE = Join-Path (Split-Path $PROFILE) -ChildPath "Scripts\gh-copilot.ps1"
-$GH_COPILOT_IMPORT_CMD = ". `"$GH_COPILOT_PROFILE`""
+# Load other configurations
+. "$PSScriptRoot\Scripts\conda_init.ps1"
 
-# Create or overwrite the gh-copilot.ps1 file
-@"
-function ghce { gh copilot explain @args }
-function ghcs { gh copilot suggest @args }
-"@ > $GH_COPILOT_PROFILE
-
-# Corrected command to check if the import command already exists in the profile
-if (-not (Select-String -Path $PROFILE -Pattern ([regex]::Escape($GH_COPILOT_IMPORT_CMD)))) {
-    $GH_COPILOT_IMPORT_CMD >> $PROFILE
-}
-
-# Import zoxide configuration
-# * If using starship, zoxide must be initialized after starship
-if (Test-Path "$PSScriptRoot\Scripts\zoxide_init.ps1") {
-    . "$PSScriptRoot\Scripts\zoxide_init.ps1"
-}
-
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
-}
-. "$PSScriptRoot\gh-copilot.ps1"
-
-. "C:\Users\braxt\.config\path.ps1"
-. "C:\Users\braxt\OneDrive\Documents\PowerShell\Scripts\gh-copilot.ps1"
-Import-Module posh-git
+# Initialize oh-my-posh
 oh-my-posh init pwsh --config 'C:\Users\braxt\OneDrive\Documents\PowerShell\Scripts\pwsh10k.omp.json' | Invoke-Expression
